76e2cef78999685b1df0143bb2e2342126a73255
Sam Gleske <sam.mxracer@gmail.com>
2025-03-21 00:49:29 -0400

    Better readme and backup fixes

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
-# Prerequisites
+# Endless Sky Offline Backup
+
+I maintain an offline backup of endless sky, its associated repositories, and
+plugin index.
+
+## Prerequisites
 
 Install `openjdk-11-jre` and `git`.
 
+Create a GitHub personal access token with no permissions and save it to
+`github-token` in the root of this repository.
+
 Run `./setup-prereqs.sh` from the root of this repository.
+
+## Backup
+
+Run the backup periodically.
+
+```bash
+export backup_scripts="/mnt/fast/endless-sky-backup"
+export backup_destination="/mnt/fast/endless-sky-mirror"
+export intended_user=root
+"$backup_scripts/backup.sh"
+```
+
+## Schedule
+
+I set up a crontab.
+
+```cron
+0 3 * * * /mnt/fast/endless-sky-backup/backup.sh
+```
--- backup.sh
+++ backup.sh
@@ -5,19 +5,28 @@
 # Linux 6.1.43 aarch64
 # GNU bash, version 5.1.16(1)-release (aarch64-unknown-linux-gnu)
 
-if [ ! "$(whoami)" = root ]; then
-  echo 'This script is expected to be run as root.'
+set -e
+
+backup_scripts="${backup_scripts:-/mnt/fast/endless-sky-backup}"
+backup_destination="${backup_destination:-/mnt/fast/endless-sky-mirror}"
+intended_user="${intended_user:-root}"
+# do not prompt for https password when checking for wikis
+GIT_TERMINAL_PROMPT="${GIT_TERMINAL_PROMPT:-0}"
+export backup_destination backup_scripts intended_user GIT_TERMINAL_PROMPT
+
+if [ ! "$(whoami)" = "$intended_user" ]; then
+  echo "This script is expected to be run as ${intended_user}."
   exit 1
 fi
 
-function is_repo_cloneable() {
-  timeout 1 git ls-remote "$1" HEAD &> /dev/null
-}
+function is_repo_cloneable() (
+  exec &> /dev/null
+  timeout 1 git ls-remote "$1" HEAD
+)
 function check_and_clone() {
-    if [ ! -d "$1" ] && is_repo_cloneable "$2"; then
-      git clone --mirror "$2"
-    fi
-
+  if [ ! -d "$1" ] && is_repo_cloneable "$2"; then
+    git clone --mirror "$2"
+  fi
 }
 function clone_repos_and_wikis() {
   java -jar cloneable.jar \
@@ -25,7 +34,7 @@ function clone_repos_and_wikis() {
     --owner=endless-sky | \
   while read -r repo; do
     (
-      cd "$backup_destination"
+      cd -- "$backup_destination"
       wiki="${repo}.wiki.git"
       bare_repo="${repo}.git"
       set -x
@@ -36,28 +45,33 @@ function clone_repos_and_wikis() {
 }
 function update_backups() {
   find . -maxdepth 1 -name '*.git' -print0 | \
-    xargs -0 -n1 -P4 -I'{}' -- /bin/bash -exc 'cd "{}"; git fetch'
+    xargs -0 -n1 -P4 -I'{}' -- /bin/bash -exc 'cd -- "{}"; git fetch'
 }
 function list_plugins() (
   PATH="/mnt/fast/endless-sky-backup:$PATH"
-  cd /mnt/fast/endless-sky-mirror/endless-sky-plugins.git
+  cd -- /mnt/fast/endless-sky-mirror/endless-sky-plugins.git
   git show HEAD:generated/plugins.yaml | yq '.[].homepage'
 )
 function clone_plugins() {
   list_plugins | while read -r repo; do
     (
-      cd "$backup_destination"
+      cd -- "$backup_destination"
+      repo="${repo%/}"
       local_wiki="${repo##*/}"
       local_wiki="${local_wiki%.git}.wiki.git"
       local_repo="${repo##*/}"
-      local_repo="${local_wiki%.git}.git"
-      check_and_clone "$local_wiki" "${repo%.git}".wiki.git
-      check_and_clone "$local_repo" "${repo%.git}".git
+      local_repo="${local_repo%.git}.git"
+      check_and_clone "${local_wiki}" "${repo%/*}/${local_wiki}"
+      check_and_clone "${local_repo}" "${repo%/*}/${local_repo}"
     )
   done
 }
 function create_reflog() (
-  cd "$backup_destination"
+  if [ -d "${backup_destination}/reflog/.git" ]; then
+    # exit subshell
+    exit 0
+  fi
+  cd -- "$backup_destination"
   if [ ! -d reflog ]; then
     mkdir reflog
   fi
@@ -70,16 +84,16 @@ function create_reflog() (
   fi
 )
 function build_reflog() (
-  cd "$backup_destination"
+  cd -- "$backup_destination"
   for x in *.git; do
     (
-      cd "$x"
+      cd -- "$x"
       git show-ref
     ) > reflog/"${x%.git}"
   done
 )
 function commit_reflog() (
-  cd "$backup_destination"/reflog
+  cd -- "$backup_destination"/reflog
   git add .
   if ! git diff --exit-code --cached &> /dev/null; then
     git commit -m "Daily show-ref of bare repositories $(date +%Y-%d-%m)"
@@ -88,11 +102,10 @@ function commit_reflog() (
 #
 # MAIN
 #
-cd /mnt/fast/endless-sky-backup/
-backup_destination=/mnt/fast/endless-sky-mirror
-export backup_destination
+cd -- "$backup_scripts"
 clone_repos_and_wikis
 clone_plugins
+update_backups
 # track refs on a daily basis
 create_reflog
 build_reflog
